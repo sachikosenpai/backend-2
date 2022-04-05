54144f31a09173b9e235d2ff7e967e52e12bea32
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-04-05 22:23:40 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace weektask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Popoola Feyisayo!");
+            Console.WriteLine("i am in backend class of wella health school!");
+            Console.WriteLine("i am learning to gain additional knowledge as well as problem solving skill!");
+        }
+    }
+}
